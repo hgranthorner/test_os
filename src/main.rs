a08ed1f1656b32ea366cc345ffc9d2aa07e1d684
Grant Horner <h.grant.horner@gmail.com>
2020-06-21 19:26:23 -0400

    Set up VGA buffer and boot loader tool chain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![no_main]
 #![no_std]
 
-fn main() {}
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
