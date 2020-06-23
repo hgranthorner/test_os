890c75a5ba93aa123cd21bd8cdfcd40c8fddbb5e
Grant Horner <h.grant.horner@gmail.com>
2020-06-22 22:02:40 -0400

    Implemented automatic panic handling

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Test panic");
     loop {}
 }
